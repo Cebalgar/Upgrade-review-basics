7381212b94bb11e91508a4a5543d159f4eeea98b
Celia Ballesteros <cebalgar@gmail.com>
2021-11-05 12:53:15 +0100

    Iteracion#3

--- Upgrade-review-basics.js
+++ Upgrade-review-basics.js
@@ -39,3 +39,60 @@ for (const user of users) {
   averages.push(user.name, avg / count);
 }
 console.log(averages);
+
+//---------------------------------------------------------------
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+for (let user of users){
+
+let sounds = user.favoritesSounds;
+
+for (let property in sounds){
+
+    console.log(property);
+   
+
+    let sonidos=[];
+
+    sonidos.push(property);
+
+    console.log(sonidos);
+
+   let repetidos={};
+   sonidos.forEach(function(numero){repetidos[numero]= (repetidos[numero]|| 0) + 1;});
+
+   console.log(repetidos);
+    
+
+
+}
+}
